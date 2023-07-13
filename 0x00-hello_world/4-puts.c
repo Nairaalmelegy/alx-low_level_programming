3a1e2a06bcc69966242b4cd52d1521cca190c665
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-07-13 15:43:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+*main - it's the main function
+*retun: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
