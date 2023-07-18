7df4fbaf9e5ecdf118c46f963c8eb34982408b55
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-07-18 10:59:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+
+	_putchar('\n');
+	i++;
+
+	}
+
+}
