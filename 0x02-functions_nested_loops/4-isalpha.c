ae154241eede602d7b315dfaedc210132147e5a9
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-07-18 12:44:40 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry function
+ * @C: the variable to be checked
+ * Return: 1 if char 0 if else
+ */
+
+int _isalpha(int c)
+{
+	if ((C >= 'A' && C <= 'Z') || (C >= 'a' && C <='z'))
+		return (1);
+	else
+		return (0);
+}
