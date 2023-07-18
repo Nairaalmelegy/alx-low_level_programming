a9ec80e00d60fc116b14ea772ad8b29f8a2a3ebe
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-07-18 10:35:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print alphabete - Entry point
+  * Return: 0 Always (success)
+  */
+void print_alphabet(void)
+{
+	char A='a';
+	while(A<='z')
+	{
+		_putchar(A);
+		A++;
+	}
+	-putchar('\n');
+	return (0);
+}
