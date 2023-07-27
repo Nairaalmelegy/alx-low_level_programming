01602a89298a01bb83672c86e28db65427e4736b
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-07-27 12:54:35 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - function for contamination
+ * @dest: function that will be contaminated to
+ * @src: function that will be contaminated from
+ *
+ * Return: void (success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i , j;
+
+	i = 0;
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while(src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
