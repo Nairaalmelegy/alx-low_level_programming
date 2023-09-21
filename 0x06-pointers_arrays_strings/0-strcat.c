aa9a641a11c1f13ee78826b20ea6845ed57e4050
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-09-21 06:20:50 -0700

    More pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
- * _strcat - function for contamination
- * @dest: function that will be contaminated to
- * @src: function that will be contaminated from
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- * Return: void (success)
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
-	while(*dest != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	while(*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-
-	*dest = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
+
