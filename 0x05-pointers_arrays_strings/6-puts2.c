42e4a4182f64a890c97e0567818fa609dfcd0dad
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-09-22 03:15:14 -0700

    0x05.pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str: character address
+ *
+ * Return: 1 or 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if(i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
