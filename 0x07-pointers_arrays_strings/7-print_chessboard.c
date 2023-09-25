aa5b5ce148e2f36f0206c2e99f3dc6aabd84d3e0
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-09-25 13:13:27 -0700

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,36 +1,18 @@
 #include "main.h"
 /**
- * print_chessboard - function to output the board shape
- * @a: array that resemples the board
- *
- * Return: 1 or 0
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	char arr[2][8] = {{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'}};
-	int i = 0;
-	char arr2[2][8] = {{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'}};
-	
-	for (; i <= 1; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = 0; j <= 7; j++)
-		{
-			a[i][j] = arr[i][j];
-		}
-	}
-	for (; i <= 5; i++)
-	{
-		for (int j = 0; j <=7; j++)
-		{
-			a[i][j] = ' ';
-		}
-	}
-	for (; i <= 7; i++)
-	{
-		for (int j = 0; j <= 7; j++)
-		{
-			a[i][j] = arr2[i][j];
-		}
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
 }
-
