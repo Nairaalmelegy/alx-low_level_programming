8801072a0e6b810978ff01f061f71e3adf5ac604
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-09-28 14:01:54 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,18 @@
 /**
  * _puts_recursion - prints a string followed by new line
  * @s: address of array
- *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_put_recursion(s + 1);
 	}
-	_putchar('\n');
-	return;
 }
 
