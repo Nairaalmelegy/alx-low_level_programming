c822681748ec21646215b33ede2d117f173c62c0
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-09-28 14:42:48 -0700

    rcursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns nmber with power the other
+ * @x: number to be multip
+ * @y: the power value
+ *
+ * Return: results of the function
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y-1))
+}
