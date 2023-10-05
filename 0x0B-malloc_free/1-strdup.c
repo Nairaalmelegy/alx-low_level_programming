70ba78f1daedb6a3c1c6eac3df7467a0111c6673
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-10-05 14:05:15 -0700

    done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - newly allocate the array
+ * @str: char that will be used
+ *
+ * Return: pointer if success or NULL if fail
+ */
+char *_strdup(char *str)
+{
+	char arr;
+	int i, length = 0;
+	if (str == 0)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	arr = (char *) malloc((sizeof(char) * length)+1);
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		arr[i] = str[i];
+
+	arr[length] = '\0';
+
+	return (arr);
+}
