36c6e78732e4f8378d60247fa4d2e6a57ec25c66
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-10-05 14:30:40 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * str_concat - concatenates two strings.
  * @s1: first string
@@ -11,28 +13,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *arr;
-	int length = 0, length2 = 0,len, i;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == 0 && s2 == 0)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
 
-	while (*s1 != '\0')
-		length++;
-	while (*s2 != '\0')
-		length2++;
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
 
-	len = length + length2;
-	arr = (char *) malloc((sizeof(char) * len) + 1);
-	if (arr == 0)
-		return (NULL);
-	for (i = 0; i < length; i++)
-		arr[i] = s1[i];
-
-	for (i = length; i < len; i++)
-		arr[i] = s2[i];
-
-	arr[len] = '\0';
-
-	return (arr);
+	return (result);
 }
