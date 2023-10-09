5c104019e0a264471b82bd7736f765c6bf6ae94e
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-10-09 10:42:49 -0700

    done more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - to allocate the memory using malloc
+ * @b: the variable address
+ *
+ * Return: pointer to arrsy or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == 0)
+		exit(98);
+	return (ptr);
+}
