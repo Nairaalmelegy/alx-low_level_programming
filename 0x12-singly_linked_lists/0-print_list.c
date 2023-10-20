e5347f820ec4140adc52f9f07ee1952a6e51e1ad
Naira <nairaalmelegy@gmail.com>
2023-10-20 03:36:18 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,35 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * print_list - to print the content of list_t
- * @h: the content of list_t
+ * File: lists.h
+ * Author: Naira almelegy
  * 
- * Return: counter number of nodes
+ * Description: project to print list
  */
+
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
 size_t print_list(const list_t *h)
 {
-    size_t counter = 0;
-    
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        
-        counter++;
-        h = h->next;
-    }
-    return (counter);
-}
+	size_t nodcount = 0;
 
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodcount++;
+		h = h->next;
+	}
+
+	return (nodcount);
+}
