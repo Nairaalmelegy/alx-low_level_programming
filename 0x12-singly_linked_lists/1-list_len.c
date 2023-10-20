3460431e8f52b2df6b6869b2795620da21ddbcdb
Naira <nairaalmelegy@gmail.com>
2023-10-20 04:41:18 -0700

    print_number

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_len - prints the number of elements in list_t list.
+ * list_len - returns the number of elements in list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
