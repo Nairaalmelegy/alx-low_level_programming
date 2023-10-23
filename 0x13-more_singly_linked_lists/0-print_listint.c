407cd98e6ef0f755310e7312ecf93632cb555b7b
Naira <nairaalmelegy@gmail.com>
2023-10-23 11:34:03 -0700

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,14 @@
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
+size_t print_listint(const listint_t * h) {
+  const listint_t * cursor = h;
+  size_t count = 0;
 
-	while (cursor != NULL)
-	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+  while (cursor != NULL) {
+    printf("%d\n", cursor -> n);
+    count += 1;
+    cursor = cursor -> next;
+  }
+  return (count);
 }
