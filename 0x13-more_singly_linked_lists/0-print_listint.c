49abb7a9525bc7172781cbce11de84ac7b4f4375
Naira <nairaalmelegy@gmail.com>
2023-10-23 11:21:44 -0700

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *ptr = h;
+	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (ptr != NULL)
+	while (cursor != NULL)
 	{
-		printf("%d\n", ptr->n);
+		printf("%d\n", cursor->n);
 		count += 1;
-		ptr = ptr->next;
+		cursor = cursor->next;
 	}
 	return (count);
 }
