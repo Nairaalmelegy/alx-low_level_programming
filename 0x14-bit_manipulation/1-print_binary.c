aeb8b08cf9efa6158ff426592835cb97f941ce3d
Naira <nairaalmelegy@gmail.com>
2023-10-27 05:13:24 -0700

    bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,14 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n >> 0)
 	{
-		printf("0");
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-
-	while (n != 0)
+	else
 	{
-		printf("%ld", n % 2);
-		n /= 2;
+		_putchar('0');
 	}
 }
