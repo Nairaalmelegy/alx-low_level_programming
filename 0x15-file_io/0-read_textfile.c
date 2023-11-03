9a6a9afda10e102311fe805c6ba77d59c07b7918
Naira <nairaalmelegy@gmail.com>
2023-11-03 05:55:02 -0700

    read from file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * read_textfile - read a n letters from file
  * @filename: the name of the file which will read from
@@ -9,7 +10,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t *fb, wrt, rd;
+	int fp, rd, wrt;
 	char *sizeOflet;
 
 	fp = open(filename, O_RDONLY);
@@ -17,8 +18,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	sizeOflet = malloc(sizeof(char) * letters);
+	if (sizeOflet == NULL)
+	{
+		close(fp);
+		return (0);
+	}
 	rd = read(fp, sizeOflet, letters);
+	if (rd == -1)
+	{
+		close(fp);
+		free(sizeOflet);
+		return (0);
+	}
 	wrt = write(STDOUT_FILENO, sizeOflet, rd);
+	if (wrt == -1)
+	{
+		close(fp);
+		free(sizeOflet);
+		return (0);
+	}
 
 	free(sizeOflet);
 	close(fp);
